1fa96f0ea6874ddbf589e261dce0da1b09b7d010
vagabon-09 <rajeshbhadra62@gmail.com>
2024-08-22 23:59:36 +0530

    * Accenture Question solution of q23 added with answer ✅

---
+++ src/Accenture/Common30/Q23.java
@@ -0,0 +1,22 @@
+package Accenture.Common30;
+
+import java.util.Arrays;
+
+public class Q23 {
+    public static void main(String[] args) {
+        String str = "Code to Crack Puzzle";
+        String reverse = getReverse(str);
+        System.out.println(reverse);
+    }
+
+    public static String getReverse(String str) {
+        String[] reverse = str.split(" ");
+        StringBuilder stb = new StringBuilder();
+        for (int i = reverse.length - 1; i >= 0; i--) {
+            if (i != 0) stb.append(reverse[i]).append(" ");
+            else stb.append(reverse[i]);
+
+        }
+        return stb.toString();
+    }
+}
