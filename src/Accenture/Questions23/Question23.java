cc683b34a5f93bf3c1bf6e323879ecbb63cd05c1
vagabon-09 <rajeshbhadra62@gmail.com>
2024-08-25 11:51:08 +0530

    * Accenture Important Question23 added with answer ✅

---
+++ src/Accenture/Questions23/Question23.java
@@ -0,0 +1,19 @@
+package Accenture.Questions23;
+
+import java.util.*;
+
+public class Question23 {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 4, 3, 9, 7, 1};
+        int output = findOutPut(arr, 9);
+        System.out.println(output);
+    }
+
+    public static int findOutPut(int[] arr, int sum) {
+        if (arr.length < 2) return -1;
+        Arrays.sort(arr);
+        if (arr[0] + arr[1] < sum) return arr[0] * arr[1];
+        return 0;
+    }
+
+}
