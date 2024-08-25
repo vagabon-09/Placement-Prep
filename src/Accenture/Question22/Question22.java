3c03d2d8f7f8b9152bcaa5f344eeadb7c6cefee4
vagabon-09 <rajeshbhadra62@gmail.com>
2024-08-25 11:36:16 +0530

    * Accenture Important Question22 added with answer ✅

--- src/Accenture/Question22/Question22.java
+++ src/Accenture/Question22/Question22.java
@@ -4,8 +4,8 @@ import java.util.*;
 
 public class Question22 {
     public static void main(String[] args) {
-        int[] arr = {3, 2, 1, 7, 5, 4};
-//        int[] arr = {1, 8, 0, 2, 3, 5, 6};
+//        int[] arr = {3, 2, 1, 7, 5, 4};
+        int[] arr = {1, 8, 0, 2, 3, 5, 6};
         int ans = getAns(arr);
         System.out.println(ans);
     }
